94fb9d007d826d9aafebd8007dc97fd2591ad1aa
Антон Давыдов <domeniqueflo@gmail.com>
2023-03-26 15:41:20 +0300

    second implementation

--- gbOOP/Program.cs
+++ gbOOP/Program.cs
@@ -5,5 +5,7 @@ class Programm
     static void Main(string[] args)
     {
         Account account = new Account();
+        account.Type = "credit";
+        Console.WriteLine(account.Type);
     }
 }
--- gbOOP/ls2/Account.cs
+++ gbOOP/ls2/Account.cs
@@ -1,15 +1,61 @@
 ﻿
-namespace gbOOP.ls2
-{
-    public class Account
-    {
-        private int _number;
-        private decimal _amount;
-        private Enum _type;
+namespace gbOOP.ls2;
 
-        Enum AccountType
+
+public class Account
+{
+    enum AccountType
+    {
+        deposit,
+        credit
+
+    }
+
+    private int _number;
+    private decimal _amount;
+    private AccountType _type;
+    
+    public int Number
+    {
+        get { return _number; }
+        set { _number = value; }
+    }
+
+    public decimal Amount
+    {
+        get { return _amount;  }
+        set { _amount = value; }
+    }
+
+    public string Type
+    {
+        get
         {
-            drposit
+            switch (_type)
+            {
+                case AccountType.deposit:
+                    return "deposit";
+                 
+                case AccountType.credit:
+                    return "credit";               
+            }
+            return String.Empty;
+        }
+        set
+        {
+            switch (value)
+            {
+                case "credit":
+                    _type = AccountType.credit;
+                    break;
+                case "deposit":
+                    _type = AccountType.deposit;
+                    break;
+                default:
+                    Console.WriteLine("Wrong entring type");
+                    break;
+            }
         }
     }
+
 }
