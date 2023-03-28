7bd3baba99e97aaaa2bd72fe4afc0350a089b611
Антон Давыдов <domeniqueflo@gmail.com>
2023-03-28 14:25:14 +0300

    update

--- gbOOP/Program.cs
+++ gbOOP/Program.cs
@@ -4,9 +4,6 @@ class Programm
 {
     static void Main(string[] args)
     {
-        Account account = new Account("credit", 12);
-        account.Deposit(100);
-        account.Withdraw(113);
-        account.GetAccountInfo();
+
     }
 }
