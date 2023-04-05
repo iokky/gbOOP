c8f9254b3263ccb8d6b1b1d30cddd08d0d8abeef
Антон Давыдов <domeniqueflo@gmail.com>
2023-04-05 14:34:29 +0300

    clear programm.cs

--- gbOOP/Program.cs
+++ gbOOP/Program.cs
@@ -1,19 +1,10 @@
-﻿using BuildingFactoryLibrary;
-using BuildingFactoryLibrary.Factory;
-using BuildingFactoryLibrary.Domain;
-
+﻿
 class Programm
 {
     static void Main(string[] args)
     {
-        BaseFactory.GetBuild(10);
-        BaseFactory.GetBuild(18,2, 4);
-        BaseFactory.GetBuild(28,12, 6);
-        BaseFactory.Remove(54267293);
 
-        foreach (var item in BaseFactory.GetMap())
-        {
-            Console.WriteLine(item);
-        }
     }
+
+
 }
