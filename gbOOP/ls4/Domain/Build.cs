abeeab715c417414a4729dae311277636e4e0727
Антон Давыдов <domeniqueflo@gmail.com>
2023-04-05 13:07:27 +0300

    revork by task

---
+++ gbOOP/ls4/Domain/Build.cs
@@ -0,0 +1,93 @@
+﻿namespace gbOOP.ls4.Domain;
+
+public class Build
+{
+    private static int _id = 1;
+    private int _number;
+    private float _higth;
+    private int _floorCount;
+    private int _flatCount;
+    private int _entranceCount;
+
+    public int Number
+    {
+        get;
+    }
+    public float Hight
+    {
+        get { return _higth; }
+        set { _higth = value; }
+    }
+
+    public int FloorCount
+    {
+        get { return _floorCount; }
+        set { _floorCount = value; }
+    }
+
+    public int FlatCount
+    {
+        get { return _flatCount; }
+        set { _flatCount = value; }
+    }
+
+    public int EntranceCount
+    {
+        get { return _entranceCount; }
+        set { _entranceCount = value; }
+    }
+
+    private void SetId()
+    {
+        _number = _id++;
+    }
+
+    /// <summary>
+    /// Многоэтажный и многоподъездный вариант сборки
+    /// </summary>
+    /// <param name="hight"></param>
+    /// <param name="floorCount"></param>
+    /// <param name="flatCount"></param>
+    /// <param name="entranseCount"></param>
+    public Build(float hight, int floorCount, int flatCount, int entranseCount)
+    {
+        SetId();
+        _higth = hight;
+        _floorCount = floorCount;
+        _flatCount = flatCount;
+        _entranceCount = entranseCount;
+    }
+
+    /// <summary>
+    /// Одноэтажный многоподъездный вариант сборки
+    /// </summary>
+    /// <param name="hight"></param>
+    /// <param name="floorCount"></param>
+    /// <param name="flatCount"></param>
+    public Build(float hight, int floorCount, int flatCount)
+    {
+        SetId();
+        _higth = hight;
+        _floorCount = floorCount;
+        _flatCount = flatCount;
+        _entranceCount = 1;
+    }
+
+    /// <summary>
+    /// Частный одноэтажный дом
+    /// </summary>
+    /// <param name="hight"></param>
+    public Build(float hight)
+    {
+        SetId();
+        _higth = hight;
+        _floorCount = 1;
+        _flatCount = 1;
+        _entranceCount = 1;
+    }
+
+    public float GetFloorHeight() => _higth / _floorCount;
+    public float GetFloatCountByEntrance() => _flatCount / _entranceCount;
+    public float GetFloatCountByFloor() => _flatCount / _floorCount;
+    public override string ToString() => $"Building_#{_number}";
+}
--- gbOOP/ls4/Factory/BaseFactory.cs
+++ gbOOP/ls4/Factory/BaseFactory.cs
@@ -8,13 +8,49 @@ namespace gbOOP.ls4.Factory;
 public class BaseFactory
 {
 
-    private Hashtable _hashtable = new Hashtable();
+    private static Hashtable _map = new Hashtable();
     private BaseFactory() { }
 
-    public static IBuild GetBuild(float hight, int floorCount, int flatCount)
+    /// <summary>
+    /// Многоэтажный и многоподъездный вариант сборки
+    /// </summary>
+    /// <param name="hight"></param>
+    /// <param name="floorCount"></param>
+    /// <param name="flatCount"></param>
+    /// <param name="entranseCount"></param>
+    /// <returns></returns>
+    public static Build GetBuild(float hight, int floorCount, int flatCount, int entranseCount)
     {
-        IBuild build = new SingleEntranceBuilding(hight, floorCount, flatCount);
+        Build build = new Build(hight, floorCount, flatCount, entranseCount);
+        _map.Add(build.GetHashCode(), build);
         return build;
     }
-    public static IBuild GetBuild(float hight, int floorCount, int flatCount, int entranseCount) => new MultiEntranceBuilding(hight, floorCount, flatCount, entranseCount);
+
+    /// <summary>
+    /// Одноэтажный многоподъездный вариант сборки
+    /// </summary>
+    /// <param name="hight"></param>
+    /// <param name="floorCount"></param>
+    /// <param name="flatCount"></param>
+    public static Build GetBuild(float hight, int floorCount, int flatCount)
+    {
+        Build build = new Build(hight, floorCount, flatCount);
+        _map.Add(build.GetHashCode(), build);
+        return build;
+    }
+
+    /// <summary>
+    /// Частный одноэтажный дом
+    /// </summary>
+    /// <param name="hight"></param>
+    public static Build GetBuild(float hight)
+    {
+        Build build = new Build(hight);
+        _map.Add(build.GetHashCode(), build);
+        return build;
+    }
+
+
+    public static Hashtable GetMap() => _map;
+    public static void Remove(int code) => _map.Remove(code);
 }
