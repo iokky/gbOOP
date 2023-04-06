be4e85084fb221b0cfc62235518d8c3e717959ed
Антон Давыдов <domeniqueflo@gmail.com>
2023-04-06 14:15:55 +0300

    crear program.cs

--- gbOOP/Program.cs
+++ gbOOP/Program.cs
@@ -1,16 +1,11 @@
-﻿using BuildingFactoryLibrary.Domain;
-using BuildingFactoryLibrary.Builder;
-
+﻿
 class Programm
 {
     static void Main(string[] args)
     {
-        HouseBuilder builder = new HouseBuilder();
-        HouseBuildDirector director = new HouseBuildDirector(builder);
 
-        director.Build(5, 10);
-        Build house = builder.GetBuild();
-        Console.WriteLine(house);
     }
+
+
 }
 
