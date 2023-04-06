f92749e274b42f396b7461721eb06840dfd735cd
Антон Давыдов <domeniqueflo@gmail.com>
2023-04-06 14:13:43 +0300

    add builder pattern implementation

--- gbOOP/Program.cs
+++ gbOOP/Program.cs
@@ -1,10 +1,16 @@
-﻿
+﻿using BuildingFactoryLibrary.Domain;
+using BuildingFactoryLibrary.Builder;
+
 class Programm
 {
     static void Main(string[] args)
     {
+        HouseBuilder builder = new HouseBuilder();
+        HouseBuildDirector director = new HouseBuildDirector(builder);
 
+        director.Build(5, 10);
+        Build house = builder.GetBuild();
+        Console.WriteLine(house);
     }
-
-
 }
+
